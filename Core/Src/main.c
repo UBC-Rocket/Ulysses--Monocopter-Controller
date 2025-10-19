0722ba3b7d91e7e724d812cf8c086339a8e17a01
Random121 <59186475+Random121@users.noreply.github.com>
2025-10-19 14:55:24 -0700

    fix(uart5): use 57600 baud and disable circular DMA for tx

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -47,8 +47,6 @@ UART_HandleTypeDef huart4;
 UART_HandleTypeDef huart5;
 UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart3;
-DMA_NodeTypeDef Node_GPDMA1_Channel3;
-DMA_QListTypeDef List_GPDMA1_Channel3;
 DMA_HandleTypeDef handle_GPDMA1_Channel3;
 DMA_NodeTypeDef Node_GPDMA1_Channel2;
 DMA_QListTypeDef List_GPDMA1_Channel2;
@@ -353,7 +351,7 @@ static void MX_UART5_Init(void)
 
   /* USER CODE END UART5_Init 1 */
   huart5.Instance = UART5;
-  huart5.Init.BaudRate = 115200;
+  huart5.Init.BaudRate = 57600;
   huart5.Init.WordLength = UART_WORDLENGTH_8B;
   huart5.Init.StopBits = UART_STOPBITS_1;
   huart5.Init.Parity = UART_PARITY_NONE;
--- Core/Src/stm32u5xx_it.c
+++ Core/Src/stm32u5xx_it.c
@@ -55,8 +55,6 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
-extern DMA_NodeTypeDef Node_GPDMA1_Channel3;
-extern DMA_QListTypeDef List_GPDMA1_Channel3;
 extern DMA_HandleTypeDef handle_GPDMA1_Channel3;
 extern DMA_NodeTypeDef Node_GPDMA1_Channel2;
 extern DMA_QListTypeDef List_GPDMA1_Channel2;
